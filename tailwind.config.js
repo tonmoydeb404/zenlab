d9a073352ef2562c4463b83dd04ffec672e04b55
Tonmoy Deb <tonmoydeb404@gamil.com>
2021-12-15 11:34:12 +0600

    removed unused file

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./*.html", "./pages/*.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        primary: "#0057C9",
+        secondary: "#1D293A",
+        offwhite: "#F6F6F6",
+        custom : {
+          red : "#FA2740",
+          green : "#008D30",
+          paste : "#15938C",
+          pink : "#FA276F",
+          orange : "#FE8A08",
+        }
+        
+      },
+      textColor: "#1D293A",
+      
+    },
+  },
+  plugins: [],
+}
